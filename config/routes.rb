00cc38f953f2abfc342e6389f84986120c93bc0f
vagrant <vagrant@localhost.localdomain>
2020-08-02 07:01:11 +0000

    modified admins and more...

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'homes#top'
+  get 'customers/show'
+  get 'customers/edit'
+  root 'home#top'
   get 'home/about'
   devise_for :customers
-  resources :customers, only: [:show, :edit, :update]
+  resource :customers, only: [:show, :edit, :update]
   get "/customers" => "customers#hide"
   get "/customers/last_conform" => "customers#last_conform"
-  resource :cart_products, only: [:index, :create, :update, :show]
+  resources :cart_products, only: [:index, :create, :update, :show]
   get "/cart_products/:id" => "cart_products#reset"
   resources :orders, only: [:new, :index, :show, :create]
   get "/orders/confirm" => "orders#confirm"
