4f96cecf39334078ef751301b0e4b6201718a193
vagrant <vagrant@localhost.localdomain>
2020-08-03 05:52:52 +0000

    20200803

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_02_013940) do
+ActiveRecord::Schema.define(version: 2020_08_02_100523) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -43,6 +42,17 @@ ActiveRecord::Schema.define(version: 2020_08_02_013940) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "discription"
+    t.integer "no_tax_price"
+    t.string "image_id"
+    t.boolean "is_sold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
