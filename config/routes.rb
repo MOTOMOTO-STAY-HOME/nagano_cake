1713cf1df293d4e34d604dc7ace8064b1df097f4
vagrant <vagrant@localhost.localdomain>
2020-08-03 02:57:31 +0000

    admin_login_modifing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,36 @@
 Rails.application.routes.draw do
-  get 'home/top'
+
+  #get"/customers"=> "customers#show,edit"がresouceとは別に存在していたので消しました。
+  root 'home#top'
   get 'home/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # devise_for :customers
+  resource :customers, only: [:show, :edit, :update]
+  patch "/customers" => "customers#hide"#get=>patch
+  get "/customers/last_confirm" => "customers#last_confirm"#conform=>confirm
+  resources :cart_products, only: [:index, :create, :update, :show]
+  delete "/cart_products/:id" => "cart_products#reset" #get=>delete
+  resources :orders, only: [:new, :index, :show, :create]
+  get "/orders/confirm" => "orders#confirm"
+  get "/orders/thanks" => "orders#thanks"
+  resource :ships, only: [:index, :show, :create, :update, :destroy]
+  resources :products, only: [:index, :show]
+
+
+  devise_for :admins, controllers:{
+    sessions: "admins/sessions",
+    passwords: "admins/passwords",
+    registrations: "admins/registrations"
+  }
+  namespace :admins do
+    get 'home/top'
+    resources :products, only: [:new, :index, :show, :edit, :create, :update]
+    patch "/products/:id" => "products#hide"#get=>patch
+    resources :product_generes, only: [:index, :edit, :create, :update]
+    resources :customers, only: [:index, :show, :edit, :update] do
+      resources :orders, only: [:index, :show, :update] do
+        resources :order_products, only: [:update]
+      end
+    end
+  end
+
 end
