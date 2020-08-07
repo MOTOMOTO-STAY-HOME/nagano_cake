a0094cc3fc41bdafc7e2a4856fb25b1ecc0ad8e0
vagrant <vagrant@localhost.localdomain>
2020-08-07 03:42:17 +0000

    Modefied CartProdut and Order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,23 +1,32 @@
 class OrdersController < ApplicationController
 
-  def new
-  	@order_payment = Order.new
-  	@ship_address = Order.new
-  end
+  before_action :authenticate_customer!
 
-  def show
+  def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to orders_confirm_path
   end
 
   def index
+  end
+
+  def show
   end
 
   def confirm
   end
 
   def thanks
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :shipping_name, :shipping_postal_code, :shipping_address, :payment_method, :total_price, :order_status, :postages_price)
   end
 
 end
