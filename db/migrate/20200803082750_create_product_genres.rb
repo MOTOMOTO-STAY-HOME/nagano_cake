9588e04f0b1a47ba71d02b4b11de840938012966
vagrant <vagrant@localhost.localdomain>
2020-08-07 05:34:08 +0000

    To pull develop200806

--- db/migrate/20200803082750_create_product_genres.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductGenres < ActiveRecord::Migration[5.2]
-  def change
-    create_table :product_genres do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200806084504_rename_product_genres_to_products_genere.rb
+++
@@ -1,5 +0,0 @@
-class RenameProductGenresToProductsGenere < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :product_genres, :products_genere
-  end
-end
