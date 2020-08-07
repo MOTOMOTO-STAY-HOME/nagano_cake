222a511bf75e00b5471b7ec712b305b6695158a2
@naoko-asano <peeeeko35@gmail.com>
2020-08-07 15:44:09 +0900

    resolve conflict

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -14,7 +14,7 @@ class ShipsController < ApplicationController
   		flash[:success]= "新しい配送先が登録されました。"
   		redirect_to ships_url
   	else
-  		flash.now[:danger]= "必須事項を正しく入力してください。"
+  		flash.now[:danger]= "必須項目を正しく入力してください。"
   		@ships = Ship.where(customer_id: current_customer.id).page(params[:page]).reverse_order
   		render :index
   	end
@@ -30,7 +30,7 @@ class ShipsController < ApplicationController
   		flash[:success]= "配送先が変更されました。"
   		redirect_to ships_url
   	else
-  		flash.now[:danger]= "必須事項を正しく入力してください。"
+  		flash.now[:danger]= "必須項目を正しく入力してください。"
   		render :edit
   	end
   end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,6 +2,6 @@ class Ship < ApplicationRecord
   belongs_to :customer
   validates :customer_id, presence: true
   validates :name, presence: :true
-  validates :postal_code, presence: :true
+  validates :postal_code, presence: :true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: :true
 end
