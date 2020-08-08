ed54939c3c5935ccddacbb4a94fb0a385b7b811d
vagrant <vagrant@localhost.localdomain>
2020-08-08 00:56:21 +0000

    2008080956

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -24,4 +24,7 @@ class Admins::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  def after_sign_in_path_for(resource)
+        admins_home_top_path
+  end
 end
