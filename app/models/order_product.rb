0b419bcf5bf9b24ce0220f427483433e10af735f
vagrant <vagrant@localhost.localdomain>
2020-08-08 23:01:16 +0000

    admins/order/show selectbox

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,6 +3,7 @@ class OrderProduct < ApplicationRecord
 	belongs_to :order
 
 	enum production_status: {true:0, false:1}
+	
 
 	validates :quantity, presence: true
 	validates :unit_price, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,47 @@ Admin.create!(
   email: "admin@admin.com",
   password: "admin10",
 )
+Customer.create!(
+  last_name: "テス",
+  first_name: "太郎",
+  kana_last_name: "tesu",
+  kana_first_name: "tarou",
+  email: "test@test.com",
+  password: "testtest",
+  postal_code: "testtest",
+  address: "testtest",
+  phone: "testtest",
+  is_active: "true",
+)
+ProductGenre.create!(
+  name: "tesuto"
+)
+Product.create!(
+  name: "test_cake",
+  discription: "koreha_tesuto_cake",
+  no_tax_price: "1000",
+  product_genre_id: 1,
+)
+Ship.create!(
+  customer_id: 1,
+  name: "test_granma",
+  postal_code: "1110000",
+  address: "test_granma_hause",
+)
+Order.create!(
+  customer_id: 1,
+  shipping_name: "test_granma",
+  shipping_postal_code: 1110000,
+  shipping_address: "test_granma_hause",
+  payment_method: 0,
+  total_price: 1100,
+  order_status: 0,
+  postages_price: 500,
+)
+OrderProduct.create!(
+  order_id: 1,
+  product_id: 1,
+  quantity: 3,
+  production_status: 0,
+  unit_price: 1600,
+)
