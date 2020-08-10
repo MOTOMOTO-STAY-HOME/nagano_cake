9a5b027181c82a93ed69b80c0d75381a78c1c4ff
vagrant <vagrant@localhost.localdomain>
2020-08-10 23:34:42 +0000

    Make ja.yml file

---
+++ app/controllers/cart_products_controller.rb
@@ -0,0 +1,47 @@
+class CartProductsController < ApplicationController
+
+	before_action :authenticate_customer!
+
+	def create
+		@cart_product = current_customer.cart_products.new(cart_product_params)
+		if @cart_product.save
+			flash[:success] = "カートに入りました。"
+			redirect_to cart_products_path
+		else
+			redirect_back(fallback_location: root_url)
+		end
+	end
+
+	def index
+		@cart_products = CartProduct.where(customer_id: current_customer.id).page(params[:page]).reverse_order
+		@total_price =0
+		@cart_products.each do |cart_product|
+			@total_price += cart_product.include_tax_total_price
+		end
+	end
+
+	def update
+		@cart_product = CartProduct.find(params[:id])
+		if @cart_product.update(cart_product_params)
+			flash[:success] = "個数を変更しました。"
+			redirect_to cart_products_path
+		end
+	end
+
+	def destroy
+		cart_product = CartProduct.find(params[:id])
+		cart_product.destroy
+		redirect_to cart_products_path
+	end
+
+	def reset
+		cart_products = CartProduct.where(customer_id: current_customer.id)
+		cart_products.destroy_all
+		redirect_to cart_products_path
+	end
+
+	private
+	def cart_product_params
+		params.require(:cart_product).permit(:customer_id, :product_id, :quantity)
+	end
+end
---
+++ app/models/cart_product.rb
@@ -0,0 +1,15 @@
+class CartProduct < ApplicationRecord
+	belongs_to :customer
+	belongs_to :product
+	validates :quantity, presence: true, format: { with: /\A\d+\z/ }
+
+	def include_tax_unit_price
+		#(self.product.no_tax_price * 1.1.to_f).ceil * self.quantity
+		 (self.product.no_tax_price * 1.1).floor
+	end
+
+	def include_tax_total_price
+		#(self.product.no_tax_price * 1.1.to_f).ceil * self.quantity
+		(self.quantity * self.product.no_tax_price * 1.1).floor
+	end
+end
